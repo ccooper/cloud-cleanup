6875e25664afaee2da22d3c1dc71d49eac04e450
Chris Cooper <ccooper@deadsquid.com>
2020-05-27 18:01:34 -0400

    Allow script to run against different AWS accounts by specifying --account on the command-line
    Specifically, this allows us to run similar reports and cleanup against
    the community cluster as for the firefoxci cluster. This assumes you
    have separate profiles setup for each account.
    
    Because MFA expiry and/or mismatched credentials are more likely now,
    wrap the AWS ops in a try block.

--- terminate_long_running_instances.py
+++ terminate_long_running_instances.py
@@ -4,6 +4,7 @@ import argparse
 import boto3
 import pprint
 import pytz
+import sys
 
 from botocore.exceptions import ClientError
 from datetime import datetime, timedelta
@@ -17,6 +18,7 @@ Here's the type of query we're tryp to replicate using boto3:
 
 # boto3.set_stream_logger('', level=1)
 pp = pprint.PrettyPrinter(indent=4)
+VALID_ACCOUNTS = ['firefoxci', 'community']
 
 
 def is_instance_terminated(instance):
@@ -27,7 +29,8 @@ def is_instance_terminated(instance):
 
 def is_instance_a_worker(instance):
     for tag in instance['Tags']:
-        if tag['Key'] == 'CreatedBy' and tag['Value'] == 'taskcluster-wm-aws':
+        if tag['Key'] == 'CreatedBy' and \
+           tag['Value'] in ['taskcluster-wm-aws', 'taskcluster-wm-community-tc-workers-aws']:
             return True
     return False
 
@@ -52,12 +55,21 @@ if __name__ == "__main__":
     parser.add_argument(
         "--dryrun", help="Dry run only, no actualy instance termination", action="store_true"
     )
+    parser.add_argument(
+        "--account",
+        type=str,
+        help="AWS account to run against. Valid options are: " + ', '.join(VALID_ACCOUNTS),
+        default=VALID_ACCOUNTS[0]
+    )
     args = parser.parse_args()
 
     DRY_RUN = False
     if args.dryrun:
         DRY_RUN = True
-    AWS_PROFILE = 'mfa'
+    if args.account not in VALID_ACCOUNTS:
+        print("ERROR: valid account options are: " + ', '.join(VALID_ACCOUNTS))
+        sys.exit(1)
+    ACCOUNT = args.account
     REGIONS = [
             'eu-central-1',
             'us-east-1',
@@ -67,31 +79,40 @@ if __name__ == "__main__":
     NUM_DAYS = 5
     date_threshold = datetime.utcnow().replace(tzinfo=pytz.utc) - timedelta(days=NUM_DAYS)
 
-    session = boto3.session.Session(profile_name=AWS_PROFILE)
+    session = boto3.session.Session(profile_name=ACCOUNT)
     for region in REGIONS:
         client = session.client('ec2',
                                 region_name=region)
         paginator = client.get_paginator('describe_instances')
+
         page_iterator = paginator.paginate()
         # filtered_iterator = page_iterator.query('Reservations[].Instances[?LaunchTime < `2020-05-10`][]')
         instances_to_terminate = []
-        for page in page_iterator:
-            for reservation in page['Reservations']:
-                for instance in reservation['Instances']:
-                    if instance['LaunchTime'] <= date_threshold:
-                        # XXX: Add other match criteria here
-                        # Only worry about instances not already marked as terminated.
-                        if is_instance_terminated(instance):
-                            continue
-                        if is_instance_a_worker(instance):
-                            print("%s: %s %s %s" % (region,
-                                                    str(instance['LaunchTime']),
-                                                    instance['InstanceId'],
-                                                    get_instance_name(instance)),
-                                  flush=True)
-                            instances_to_terminate.append(instance['InstanceId'])
 
-        print("%s: %d instances found" % (region, len(instances_to_terminate)), flush=True)
+        try:
+            for page in page_iterator:
+                for reservation in page['Reservations']:
+                    for instance in reservation['Instances']:
+                        if instance['LaunchTime'] <= date_threshold:
+                            # XXX: Add other match criteria here
+                            # Only worry about instances not already marked as terminated.
+                            if is_instance_terminated(instance):
+                                continue
+                            if is_instance_a_worker(instance):
+                                print("%s: %s %s %s" % (region,
+                                                        str(instance['LaunchTime']),
+                                                        instance['InstanceId'],
+                                                        get_instance_name(instance)),
+                                      flush=True)
+                                instances_to_terminate.append(instance['InstanceId'])
+        except ClientError as e:
+            print("ERROR: %s - is your MFA token current?" % e)
+            sys.exit(1)
+
+        print("%s: %d instance%s found" % (region,
+                                           len(instances_to_terminate),
+                                           '' if len(instances_to_terminate) == 1 else 's'),
+              flush=True)
         for id in instances_to_terminate:
             try:
                 print('%s: Attempting to delete instance %s...' % (region, id), end='')
